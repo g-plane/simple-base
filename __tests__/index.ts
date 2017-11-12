349d31687846ccf5725f76e508077d153f356eb6
Pig Fang <g-plane@hotmail.com>
2017-11-12 17:31:25 +0800

    Add more tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,17 +2,21 @@ import encode from '../src/encode'
 import decode from '../src/decode'
 
 test('base36 encode', () => {
+  expect(encode('', 36)).toBe('')
   expect(encode('hello', 36)).toBe('5pzcszu7')
 })
 
 test('base36 decode', () => {
+  expect(decode('', 36)).toBe('')
   expect(decode('5pzcszu7', 36)).toBe('hello')
 })
 
 test('base58 encode', () => {
+  expect(encode('', 58)).toBe('')
   expect(encode('hello', 58)).toBe('Cn8eVZg')
 })
 
 test('base58 decode', () => {
+  expect(decode('', 58)).toBe('')
   expect(decode('Cn8eVZg', 58)).toBe('hello')
 })
