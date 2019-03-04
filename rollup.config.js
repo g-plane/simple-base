8d9c3fc29cced3d1b8fc439807a149fcce2e47c2
Pig Fang <g-plane@hotmail.com>
2019-03-04 17:06:27 +0800

    upgrade build-related

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import typescript from 'rollup-plugin-typescript2'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/simple-base.js',
+    file: 'dist/index.js',
     format: 'umd',
+    name: 'SimpleBase',
   },
-  name: 'SimpleBase',
   plugins: [
     typescript(),
     uglify(),
